30e9e506438e709b0fd61449c1d6127738ec2d51
hunami <faqihahabdkarim@gmail.com>
2026-04-28 16:20:46 +0800

    navbar

---
+++ user/components/navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar">
+  <div class="container">
+
+    <div class="logo">
+      <img src="assets/images/logo.png" alt="Logo">
+      <span>Galeri Seramik Pasir Gudang</span>
+    </div>
+
+    <ul class="nav-links">
+      <li><a href="#">Utama</a></li>
+      <li><a href="#">Pakej</a></li>
+      <li><a href="#">Aktiviti</a></li>
+      <li><a href="produk.php">Produk</a></li>
+      <li><a href="galeri.php">Galeri</a></li>
+    </ul>
+
+    <div class="nav-action">
+      <a href="#" class="btn-primary">Tempah</a>
+    </div>
+
+  </div>
+</nav>
