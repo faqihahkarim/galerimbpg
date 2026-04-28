5e1e4ad101b78edc46f817316a1034e29eba9ba2
hunami <faqihahabdkarim@gmail.com>
2026-04-28 16:35:51 +0800

    navbar + index initial code

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Galeri Seramik Pasir Gudang</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap" rel="stylesheet">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="assets/css/navbar.css">
+</head>
+
+<body>
+
+  <!-- INCLUDE NAVBAR -->
+  <?php include 'components/navbar.php'; ?>
+
+  <!-- TEST CONTENT -->
+  <div style="padding: 50px;">
+    <h1>Index Page Content Here</h1>
+  </div>
+
+</body>
+</html>
