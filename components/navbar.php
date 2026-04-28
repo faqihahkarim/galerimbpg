95f49bafb9ff0b8689d80660f3345ea47ee1c2d1
hunami <faqihahabdkarim@gmail.com>
2026-04-28 16:09:34 +0800

    updated new folder and files (assets, components)

---
+++ components/navbar.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Galeri Seramik Majlis Bandaraya Pasir Gudang</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;500;700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <!-- Favicon -->
+  <link href="pic/logo2.png" sizes="32x16" rel="shortcut icon" />
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="landingcss.css" />
+</head>
