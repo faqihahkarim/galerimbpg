acbf681ee6d0b8afd5c0f3d965b64dafe1930771
hunami <faqihahabdkarim@gmail.com>
2026-05-04 15:55:54 +0800

    table price for pendidikan

---
+++ package/tempahan_pendidikan.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Galeri Seramik Pasir Gudang</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="../assets/css/navbar.css">
+  <link rel="stylesheet" href="../assets/css/index.css">
+  <link rel="stylesheet" href="package.css">
+
+  <!-- Favicon -->
+  <link rel="icon" href="../assets/images/logogaleri.png" type="image/png" style="width: 32px; height: 32px;">
+</head>
+
+<body>
+
+  <!-- include for components-->
+  <?php include 'navbar.php'; ?>
+
+</body>
+</html>
