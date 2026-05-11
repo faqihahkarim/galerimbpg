cf055c728dc3fd49b00d2d55724c06ff75a4269d
hunami <faqihahabdkarim@gmail.com>
2026-05-11 16:14:24 +0800

    Update navbar.php

--- components/navbar.php
+++ components/navbar.php
@@ -32,7 +32,7 @@
 
           <?php else: ?>
 
-            <a href="admin/login.php" class="btn-primary">Admin</a>
+            <a href="<?= $base ?>../admin/login.php" class="btn-primary">Admin</a>
 
           <?php endif; ?>
         </div>
